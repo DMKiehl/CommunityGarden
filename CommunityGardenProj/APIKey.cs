dbfd84890fd63c541ef0ea39d8d77aae6d46ed5d
Dana Kiehl <dana.palm87@gmail.com>
2020-06-08 11:53:17 -0500

    Created an APIKey class to store our our api Keys. Added the google maps api key.

---
+++ CommunityGardenProj/APIKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunityGardenProj
+{
+    public static class APIKey
+    {
+
+        public static readonly string GoogleMapsAPI = "AIzaSyArAXAhvA7EggoDoqL4_0G1W7QJRf7A9xs";
+
+
+    }
+}
