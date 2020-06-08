752086bd98ac45fe05730daa3975439e16b6d4da
FlyerB <zarandazchi@gmail.com>
2020-06-08 11:30:02 -0500

    gardner model complete

---
+++ CommunityGardenProj/Models/Gardener.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunityGardenProj.Models
+{
+    public class Gardener
+    {
+        [Key]
+        public int gardenerId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public int latitude { get; set; }
+
+        public double longitude { get; set; }
+        
+
+        [ForeignKey("addressId")]
+        public Address address { get; set; }
+        public double addressId { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
