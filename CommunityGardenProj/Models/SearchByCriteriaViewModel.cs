0c59694ad0f5fdae880136fa9073be27309de52f
Dana Kiehl <dana.palm87@gmail.com>
2020-06-11 18:17:05 -0500

    removed the requirement to enter input in the SearchByCriteria View

--- CommunityGardenProj/Models/SearchByCriteriaViewModel.cs
+++ CommunityGardenProj/Models/SearchByCriteriaViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,15 +8,15 @@ namespace CommunityGardenProj.Models
 {
     public class SearchByCriteriaViewModel
     {
-        public int ZipCode { get; set; }
+        public int? ZipCode { get; set; }
 
-        public int Cost { get; set; }
+        public int? Cost { get; set; }
 
         public bool VolunteerOpportunities { get; set; }
 
         public bool Organic { get; set; }
 
-        public string PlotSize { get; set; }
+        public string? PlotSize { get; set; }
 
         public bool SearchByCost { get; set; }
 
