3599d371ed4ec28298c949ae8a59eef320ac01d1
danamurphy48 <danamurphy48@gmail.com>
2020-06-11 15:49:50 -0500

    Modified some display attributes: SearchByCriteriaViewModel, Gardener Views Edit, GardenDetails, ListGardensByInterest, SearchByCriteria. Added more resources.

--- CommunityGardenProj/Models/SearchByCriteriaViewModel.cs
+++ CommunityGardenProj/Models/SearchByCriteriaViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,24 +8,25 @@ namespace CommunityGardenProj.Models
 {
     public class SearchByCriteriaViewModel
     {
+        [Display(Name = "Zip Code")]
         public int ZipCode { get; set; }
 
         public int Cost { get; set; }
-
+        [Display(Name = "Volunteer Opportunities")]
         public bool VolunteerOpportunities { get; set; }
 
         public bool Organic { get; set; }
-
+        [Display(Name = "Plot Size")]
         public string PlotSize { get; set; }
-
+        [Display(Name = "Search By Cost")]
         public bool SearchByCost { get; set; }
-
+        [Display(Name = "Search By Zip Code")]
         public bool SearchByZipCode { get; set; }
-
+        [Display(Name = "Search By Volunteer Opportunities")]
         public bool SearchByVolunteerOpportunities { get; set; }
-
+        [Display(Name = "Show Only Organic Gardens")]
         public bool SearchByOrganic { get; set; }
-
+        [Display(Name = "Search Gardens By Plot Size")]
         public bool SearchByPlotSize { get; set; }
 
         // bool property for each of the above possible search criteria
