d213bac49889729ab74ca0276a44f0f469d1ec60
Jackson Smith <jacksonsmith7343@gmail.com>
2020-06-11 10:28:04 -0500

    Added a search by criteria method.

--- CommunityGardenProj/Controllers/GardenersController.cs
+++ CommunityGardenProj/Controllers/GardenersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
 using CommunityGardenProj.ActionFilters;
 using CommunityGardenProj.Contracts;
@@ -70,17 +71,59 @@ namespace CommunityGardenProj.Controllers
 
         }
 
-        public IQueryable<T> SearchByCriteria()//location(Zip Code?), cost, volunteer opportunities, organic vs. non-organic, plotsize 
+
+        // GET
+        public ActionResult SearchByCriteria()//loads a page where user inputs values
         {
-            var gardens = GetAllGardens();
+            SearchByCriteriaViewModel search = new SearchByCriteriaViewModel();
 
-            //var searchByLocation = gardens.Result.Where(g => g.zip ==)
-            //var lowCostGardens = gardens.Result.Where(g => g.cost ).;
-            //var hasVolunteerOpportunities = gardens.Result.Where(g => g.volunteerOpportunities == true).ToDictionary;
-            //var isOrganic = gardens.Result.Where(g => g.organic == true).ToList();
-            //var SmallPlotSize = gardens.Result.Where(g => g.plotSize == )
-
+            return View();
         }
+
+
+        [HttpPost]
+        public async Task<ActionResult> SearchByCriteria(SearchByCriteriaViewModel model) //query with the user input
+        {
+
+            var gardens = await GetAllGardens();
+
+            if(model.SearchByCost == true) 
+            {
+                gardens = gardens.Where(g => g.cost == model.Cost).ToList();
+            }
+
+            if (model.SearchByOrganic == true)
+            {
+                gardens = gardens.Where(g => g.organic == model.Organic).ToList();
+            }
+
+            if (model.SearchByPlotSize == true)
+            {
+                gardens = gardens.Where(g => g.plotSize == model.PlotSize).ToList();
+            }
+
+            if (model.SearchByZipCode == true)
+            {
+                gardens = gardens.Where(g => g.zip == model.ZipCode).ToList();
+            }
+
+            if (model.SearchByVolunteerOpportunities == true)
+            {
+                gardens = gardens.Where(g => g.volunteerOpportunities == model.VolunteerOpportunities).ToList();
+            }
+
+            DisplaySearchResult(gardens);
+
+            return View();
+        }
+
+        public ActionResult DisplaySearchResult(List<Garden> gardens)
+        {
+
+            return View(gardens);
+        }
+
+
 
         // GET: GardenersController/Details/5
         public async Task<IActionResult> Details(int? id)
